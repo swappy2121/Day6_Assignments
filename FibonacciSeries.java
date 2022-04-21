c1574e4f7ce9109c9c4dbd0a90980d0dd1fe0606
swapnil <sopiswarrinku77@gmail.com>
2022-04-21 15:56:09 -0700

    Added : fibonaccy series

---
+++ FibonacciSeries.java
@@ -0,0 +1,31 @@
+package com.Bridgelabz;
+
+
+public class FibonacciSeries {
+    public static void main (String[] args) {
+        System.out.println("this is fibonacci series : ");
+        System.out.println("we have values of 0 nad 1  ");
+
+        int A = 0;
+        int B = 1;
+        int C = A+B;
+        int D = C+B;
+        int E = D+C;
+        int F = E+D;
+        int G = F+E;
+        int H = G+F;
+        int I = H+G;
+        int J = I+H;
+        int K = J+I;
+
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+        System.out.println(F);
+        System.out.println(G);
+        System.out.println(H);
+        System.out.println(I);
+        System.out.println(J);
+        System.out.println(K);
+    }
+}
