dfdb1632d210ed23c56c6e680764c3249e971246
swapnil <sopiswarrinku77@gmail.com>
2022-04-21 23:15:59 -0700

    Added : getting coupon numbers by random numbers

---
+++ CouponNumbers.java
@@ -0,0 +1,35 @@
+package com.Bridgelabz;
+
+
+public class CouponNumbers {
+
+    public static void main(String[] args) {
+        int NUM = (int) Math.floor(Math.random() * 6);
+
+        int COUPON1 = 185620;
+        int COUPON2 = 163467;
+        int COUPON3 = 172390;
+        int COUPON4 = 128690;
+        int COUPON5 = 139870;
+
+        if( NUM == 1){
+            System.out.println("your coupon number is : "+ COUPON1);
+        }else if( NUM == 2){
+            System.out.println("your coupon number is :"+ COUPON2);
+        }else if( NUM == 3){
+            System.out.println("your coupon number is :"+ COUPON3);
+        }else if( NUM == 4){
+            System.out.println("your coupon number is :"+ COUPON4);
+        }else if( NUM == 5){
+            System.out.println("your coupon number is :"+ COUPON5);
+        }
+    }
+    }
+
+
+   
+
+
+
+
+
